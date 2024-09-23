f5aa1ec174d68f5d6599e53f292aa4ecbc6e3aa1
alexis <alexisrob@netc.fr>
2024-09-23 16:57:55 +0200

    Création du fichier olympiadeservice

---
+++ src/main/java/bts/sio/webapp/service/OlympiadeService.java
@@ -0,0 +1,21 @@
+package bts.sio.webapp.service;
+
+import bts.sio.webapp.model.Olympiade;
+import bts.sio.webapp.repository.OlympiadeProxy;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Data
+@Service
+public class OlympiadeService {
+
+    @Autowired
+    private OlympiadeProxy OlympiadeProxy;
+    @Autowired
+    private OlympiadeProxy olympiadeProxy;
+
+    public Iterable<Olympiade> getLesOlympiades() {
+        return olympiadeProxy.getLesOlympiades();
+    }
+}
